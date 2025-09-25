6911ec3e2cbc9643fa4a3152879a970321612b24
sycohuesca <ajlucea@hotmail.com>
2025-09-25 18:31:56 +0200

    logo

--- src/config.ts
+++ src/config.ts
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/taz.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Sycohuesca",
+	bio: "Si se enciende y se apaga, me gusta",
 	links: [
 		{
 			name: "Twitter",
